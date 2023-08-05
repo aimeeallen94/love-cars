1897a2a72d31b9bfcc88139b2d5fd7bd2f1e9ffb
aimeeallen94 <>
2023-08-05 13:41:04 +0000

    create function to assign unique id to each column, create function to remove duplcaite rows in sheet and create funciton to target each column in sheet

---
+++ setup.py
@@ -0,0 +1,55 @@
+import gspread
+import uuid
+import pandas as pd
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_cars_2023')
+
+"""
+Removing duplicates from datasheet to prevent errors during analysis
+"""
+
+spreadsheet = SHEET.worksheet('cars').get_all_values()
+# new_spreadsheet = spreadsheet.drop_duplicates(subset=None, keep='first', inplace=False)
+# print(new_spreadsheet)
+
+def targeting_each_column():
+    """
+    Targeting each column in the spreadsheet to access for data analysis
+    """
+
+    if unique_identifier == cars[0]:
+        self.car_make = cars[1]
+        self.car = cars[2]
+        self.year = cars[3]
+        self.body_type = cars[4]
+        self.colour = cars[5]
+        self.fuel_type = cars[6]
+        self.transmission_type = cars[7]
+        self.top_speed = cars[8]
+        self.price = cars[9]
+        self.rating = cars[10]
+        self.sales = cars[11]
+
+
+def creating_uuid():
+    """
+    Creating unique identifier for each row entry in spreadsheet
+    """
+    sheet = SHEET.worksheet('cars')
+
+
+    for row, col in enumerate(spreadsheet):
+        id = uuid.uuid1()
+        sheet.update_cell(row + 1, 1, str(id))
+    
+creating_uuid()
